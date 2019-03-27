ce76b91c1bd471f95ce3139401e45e82d86d3d32
cschweda <cschweda@gmail.com>
2019-03-27 10:42:40 -0500

    fix: remove check for raw-loader

--- nuxt.config.js
+++ nuxt.config.js
@@ -87,14 +87,12 @@ module.exports = {
     ** You can extend webpack config here
     */
     extend(config, ctx) {
-      if (ctx.isDev && ctx.isClient) {
-        config.module.rules.push({
-          // enforce: 'pre',
-          test: /\.md$/i,
-          loader: 'raw-loader',
-          exclude: /(node_modules)/
-        })
-      }
+      config.module.rules.push({
+        // enforce: 'pre',
+        test: /\.md$/i,
+        loader: 'raw-loader',
+        exclude: /(node_modules)/
+      })
     }
   }
 }
